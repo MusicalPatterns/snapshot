bdbaad0a929517653dc94c6c6acd167716bd8fc6
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-06 16:18:29 -0800

    1.0.6: bit of finesse

--- bin/postinstall.sh
+++ bin/postinstall.sh
@@ -2,7 +2,7 @@
 
 set +e
 
-. ../../../node_modules/@musical-patterns/cli/bin/non_cli/run_only_if_not_self_installing.sh
+. ../../../node_modules/@musical-patterns/cli/bin/non_cli/run_only_if_not_self_installing.sh > /dev/null 2>&1
 if [[ $? == 0 ]] ; then
 	run_only_if_not_self_installing "bash ./bin/share_config.sh"
 fi
