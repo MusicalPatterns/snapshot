994576cf16e3eb9a01f8bf93afa207da67e4c3c9
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-14 17:46:01 -0800

    1.0.14: snapshot works with more advanced pattern spec

--- bin/snapshot.js
+++ bin/snapshot.js
@@ -4,9 +4,17 @@ require(`${ESCAPE_COMPILER_NODE_MODULES_FOR_PATTERN_FOR_REQIRING}test/mockDom`)
 const { appendFileSync, existsSync, unlinkSync } = require('fs')
 const { compilePattern } = require('@musical-patterns/compiler')
 
+const extractInitialSettledPatternSpec = spec =>
+    Object.entries(spec).reduce(
+        (initialSettledPatternSpec, [ key, val ]) => ({ ...initialSettledPatternSpec, [ key ]: val.initial }),
+        {},
+    )
+
 const updateSnapshot = async () => {
     const { pattern } = require(`${ESCAPE_COMPILER_NODE_MODULES_FOR_PATTERN_FOR_REQIRING}src`)
-    const snapshot = await compilePattern(pattern)
+    const { spec, material } = pattern
+    const initialSettledPatternSpec = extractInitialSettledPatternSpec(spec)
+    const snapshot = await compilePattern({ material, spec: initialSettledPatternSpec })
 
     const snapshotFile = 'src/snapshot.json'
     existsSync(snapshotFile) && unlinkSync(snapshotFile)
