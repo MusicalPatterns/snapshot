754a090506eb78d045b14391bd1adc1cb1a4b27f
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-25 09:22:57 -0800

    1.0.36: consolidate share and ignore

--- bin/postinstall.sh
+++ bin/postinstall.sh
@@ -3,13 +3,6 @@
 export CLI_DIR=../../../node_modules/@musical-patterns/cli
 
 . ${CLI_DIR}/bin/sharing/share_files.sh
-. ${CLI_DIR}/bin/sharing/ignore_files.sh
 . ${CLI_DIR}/bin/sharing/run_only_if_not_self_installing.sh
 
-share_config() {
-	share_files
-	ignore_files
-}
-export -f share_config
-
-run_only_if_not_self_installing share_config
+run_only_if_not_self_installing share_files
