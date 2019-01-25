7b9555a3e3183f7ae633a8360eb6696d57bf3ae4
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-24 17:07:25 -0800

    1.0.30: collapse share_config into postinstall

--- bin/postinstall.sh
+++ bin/postinstall.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
-set +e
+export CLI_DIR=../../../node_modules/@musical-patterns/cli
 
-. ../../../node_modules/@musical-patterns/cli/bin/non_cli/run_only_if_not_self_installing.sh > /dev/null 2>&1
-if [[ $? == 0 ]] ; then
-	run_only_if_not_self_installing "bash ./bin/share_config.sh"
-fi
+. ${CLI_DIR}/bin/non_cli/share_files.sh
+. ${CLI_DIR}/bin/non_cli/ignore_files.sh
+. ${CLI_DIR}/bin/non_cli/run_only_if_not_self_installing.sh
+
+share_config() {
+	share_files
+	ignore_files
+}
+export -f share_config
+
+run_only_if_not_self_installing share_config
--- bin/share_config.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-export CLI_DIR=../../../node_modules/@musical-patterns/cli
-
-. ${CLI_DIR}/bin/non_cli/share_files.sh
-. ${CLI_DIR}/bin/non_cli/ignore_files.sh
-
-share_files
-ignore_files
