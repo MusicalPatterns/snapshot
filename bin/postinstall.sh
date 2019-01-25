960046b698d038f711f18cdd76fd71346c1171a0
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-24 17:44:46 -0800

    1.0.31: highlight sharing bing

--- bin/postinstall.sh
+++ bin/postinstall.sh
@@ -2,9 +2,9 @@
 
 export CLI_DIR=../../../node_modules/@musical-patterns/cli
 
-. ${CLI_DIR}/bin/non_cli/share_files.sh
-. ${CLI_DIR}/bin/non_cli/ignore_files.sh
-. ${CLI_DIR}/bin/non_cli/run_only_if_not_self_installing.sh
+. ${CLI_DIR}/bin/sharing/share_files.sh
+. ${CLI_DIR}/bin/sharing/ignore_files.sh
+. ${CLI_DIR}/bin/sharing/run_only_if_not_self_installing.sh
 
 share_config() {
 	share_files
