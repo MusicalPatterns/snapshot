868a87e1fc3690484c8bc906ef750ffcf2e0e4ee
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-02-06 12:55:51 -0800

    1.0.45: temporary diagnosing moeom travis failures

--- share/test/snapshot.test.ts
+++ share/test/snapshot.test.ts
@@ -2,7 +2,7 @@ import { compilePattern } from '@musical-patterns/compiler'
 import * as path from 'path'
 
 describe('snapshot', () => {
-    // tslint:disable-next-line no-require-imports
+    // tslint:disable-next-line:no-require-imports
     const { pattern, snapshot } = require('../src/indexForTest')
 
     if (!pattern) {
@@ -18,6 +18,14 @@ describe('snapshot', () => {
     }
     else {
         it('stays locked down', async (done: DoneFn) => {
+            console.log('################ ACTUAL')
+            console.log(JSON.stringify(
+                await compilePattern({ material: pattern.material, spec: pattern.specData.initial }),
+                undefined,
+                2,
+            ))
+            console.log('################ EXPECTED')
+            console.log(JSON.stringify(snapshot, undefined, 2))
             expect(JSON.stringify(
                 await compilePattern({ material: pattern.material, spec: pattern.specData.initial }),
                 undefined,
