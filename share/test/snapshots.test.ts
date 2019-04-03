1124e407e7e5e82655351591538cae8524d43fdc
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-04-03 14:42:18 -0700

    1.0.69: snapshots are separate files - of course i didnt think id get awway with it that easily

--- share/test/snapshots.test.ts
+++ share/test/snapshots.test.ts
@@ -1,6 +1,5 @@
 import { compilePattern } from '@musical-patterns/compiler'
 import { Preset } from '@musical-patterns/pattern'
-import { CompiledPattern } from '@musical-patterns/performer'
 import { ObjectOf } from '@musical-patterns/utilities'
 import * as path from 'path'
 
@@ -8,23 +7,22 @@ describe('snapshots', () => {
     // tslint:disable-next-line no-require-imports
     const { pattern } = require('../src/indexForTest')
 
-
     const presetsSnapshotTests: (presets: ObjectOf<Preset>) => Promise<void[]> =
         async (presets: ObjectOf<Preset>): Promise<void[]> =>
             Promise.all(Object.entries(presets)
                 .map(async ([ presetName, preset ]: [ string, Preset ]) => {
                     it(`${presetName} preset stays locked down`, async (done: DoneFn) => {
-                        const { voices } = await compilePattern({ material: pattern.material, specs: preset.specs })
-
-                        // tslint:disable-next-line no-require-imports no-reaching-imports
-                        const presetSnapshot: CompiledPattern = require(`../snapshots/${presetName}`)
-
                         expect(JSON.parse(JSON.stringify(
-                            voices,
+                            await compilePattern({ material: pattern.material, specs: preset.specs }),
                             undefined,
                             2,
                         )))
-                            .toEqual(JSON.parse(JSON.stringify(presetSnapshot, undefined, 2)))
+                            .toEqual(JSON.parse(JSON.stringify(
+                                // tslint:disable-next-line no-require-imports no-reaching-imports
+                                require(`../snapshots/${presetName}`),
+                                undefined,
+                                2,
+                            )))
 
                         done()
                     })
@@ -44,17 +42,17 @@ describe('snapshots', () => {
     }
     else {
         it('initial stays locked down', async (done: DoneFn) => {
-            const { voices } = await compilePattern(pattern)
-
-            // tslint:disable-next-line no-require-imports no-reaching-imports
-            const initialSnapshot: CompiledPattern = require('../snapshots/initial')
-
             expect(JSON.parse(JSON.stringify(
-                voices,
+                await compilePattern(pattern),
                 undefined,
                 2,
             )))
-                .toEqual(JSON.parse(JSON.stringify(initialSnapshot, undefined, 2)))
+                .toEqual(JSON.parse(JSON.stringify(
+                    // tslint:disable-next-line no-require-imports no-reaching-imports
+                    require('../snapshots/initial'),
+                    undefined,
+                    2,
+                )))
 
             done()
         })
